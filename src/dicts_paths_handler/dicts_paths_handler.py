0306094ce44d8bb014c30fbf712cf2f1a0c9253f
SoloEnder <houngbedjileonide@protonmail.com>
2026-06-03 16:20:22 +0200

    docs: improved methods docstrings

--- src/dicts_paths_handler/dicts_paths_handler.py
+++ src/dicts_paths_handler/dicts_paths_handler.py
@@ -7,12 +7,21 @@ class DictsPathsHandler():
 
     def get_value(self, dict_path: str):
         """
-        Get the value of a dict value tting by it's dict path and return it.
+        Get the value of a dict value by it's dict path and return it.
         setting path must have the following format:
         keyA.keyAA.keyAAA, etc. For exemple : 
         {"general":{"ui":{"theme":"dark"}}
-        in this case, the dict path path of "theme" will be :
+        in this case, the dict path of "theme" will be :
         "general.ui.theme", and this method will return "dark"
+        
+        Parameters
+        ----------
+        dict_path: str
+            A valid dict_path (following the format defined above)
+            
+        Raises
+        ------
+        InvalidDcitPathError: if 'dict_path' isn't a valid dict_path
         """
         path_sections = dict_path.split(".")
         current_value = self.base_dict.copy()
@@ -27,7 +36,7 @@ class DictsPathsHandler():
         else:
             return current_value
     
-    def edit_value(self, dict_path, new_value):
+    def edit_value(self, dict_path, new_value)-> None:
         """
         Replace the value of a key in the dict by <new_value>
         dict_path must have the following format:
@@ -35,6 +44,18 @@ class DictsPathsHandler():
         {"general":{"ui":{"theme":"dark"}}
         in this case, the dict path of "theme" will be :
         "general.ui.theme"
+        
+        Parameters
+        ----------
+        dict_path: str
+            A valid dict_path (following the format defined above)
+            
+        new_value
+            The new value that will be assigned to 'dict_path'
+            
+        Raises
+        ------
+        InvalidDcitPathError: if 'dict_path' isn't a valid dict_path
         """
         path_sections = dict_path.split(".")
         current_value = self.base_dict.copy()
